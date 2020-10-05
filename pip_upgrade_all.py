5794e7c3f3d0781aa9a18bf45493c56aa8e9e847
Michael <SunsetOrange@users.noreply.github.com>
2020-10-06 01:03:30 +1000

    Update pip_upgrade_all.py
    Moved code into a function so it can be imported and run on demand.

--- pip_upgrade_all.py
+++ pip_upgrade_all.py
@@ -3,19 +3,28 @@ import os
 import subprocess
 
 
-try:
-	is_admin = os.getuid() == 0
-except AttributeError:
-	import ctypes
-	is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
-if is_admin:
-	# Update pip, setuptools and wheels before importing pkg_resources.
-	subprocess.call("python -m pip install --upgrade pip wheel")
-	subprocess.call("python -m pip uninstall setuptools")  # setuptools install is often broken. Uninstall first..
-	subprocess.call("python -m pip install setuptools")  # ..then reinstall to fix issues with pkg_resources.
-	import pkg_resources
-	for dist in pkg_resources.working_set:
-		subprocess.call("python -m pip install --upgrade " + dist.project_name, shell=True)
-else:
-	print("Limited privileges detected!\nPlease rerun this script with admin level privileges.")
-	input("\nPress Enter to exit.")
+def run():
+	try:
+		is_admin = os.getuid() == 0
+	except AttributeError:
+		import ctypes
+		is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+
+	if is_admin:
+		# Update pip, setuptools and wheels before importing pkg_resources.
+		subprocess.call("python -m pip install --upgrade pip wheel")
+		subprocess.call("python -m pip uninstall setuptools")  # setuptools install is often broken. Uninstall first..
+		subprocess.call("python -m pip install setuptools")  # ..then reinstall to fix issues with pkg_resources.
+		import pkg_resources
+		for dist in pkg_resources.working_set:
+			subprocess.call("python -m pip install --upgrade " + dist.project_name, shell=True)
+
+	return is_admin
+
+
+if __name__ == "__main__":
+	if run():
+		print("\nPackages have been updated.")
+	else:
+		print("Limited privileges detected!\nPlease rerun this script with admin level privileges.")
+		input("\nPress Enter to exit.")
