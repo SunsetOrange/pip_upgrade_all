ea24ee1cb835c41a9edc354a2039dcb9d816cefd
Michael <michael.bedford@superloop.com>
2021-08-02 00:33:02 +1000

    Update pip_upgrade_all.py

--- pip_upgrade_all.py
+++ pip_upgrade_all.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python3
 import os
+import sys
 import subprocess
 
 
-def run():
-	try:
-		is_admin = os.getuid() == 0
-	except AttributeError:
-		import ctypes
-		is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
-
+def run(is_admin: bool):
+	subprocess.call(["python3", "-m", "pip", "install", "--upgrade", "pip", "wheel"])
 	if is_admin:
-		# Update pip, setuptools and wheels before importing pkg_resources.
-		subprocess.call("python -m pip install --upgrade pip wheel")
-		subprocess.call("python -m pip uninstall setuptools")  # setuptools install is often broken. Uninstall first..
-		subprocess.call("python -m pip install setuptools")  # ..then reinstall to fix issues with pkg_resources.
-		import pkg_resources
-		for dist in pkg_resources.working_set:
-			subprocess.call("python -m pip install --upgrade " + dist.project_name, shell=True)
+		subprocess.call(["python3", "-m", "pip", "uninstall", "-y", "setuptools"])  # setuptools install is often broken. Uninstall first..
+	subprocess.call(["python3", "-m", "pip", "install", "setuptools"])  # ..then reinstall to fix issues with pkg_resources.
 
-	return is_admin
+	# Update pip, setuptools and wheels before importing pkg_resources.
+	import pkg_resources
+	for dist in pkg_resources.working_set:  # pylint: disable=not-an-iterable
+		project_name = dist.project_name
+		subprocess.call(["pip", "install", "--upgrade", project_name])
 
 
 if __name__ == "__main__":
-	if run():
-		print("\nPackages have been updated.")
+	is_admin = False
+	if "admin" in sys.argv:
+		try:
+			is_admin = os.getuid() == 0
+		except AttributeError:
+			import ctypes
+			is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+
+	if is_admin:
+		print("Running script in admin mode.")
 	else:
-		print("Limited privileges detected!\nPlease rerun this script with admin level privileges.")
-		input("\nPress Enter to exit.")
+		print("Running script in local mode.")
+
+	run(is_admin)
+	print("\nPackages have been updated.")
+	input("\nPress Enter to exit.")
